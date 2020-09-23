879ffc49358ad77d88212e3bc0fc445b41c5bfd8
Brad Jones <brad.jones@fruition.net>
2020-09-22 22:38:36 -1000

    Revert "symlinks appear unnecessary?"
    This reverts commit faa2f580ebaf8939a9bae9f526a3902c1536381c.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,15 @@ fi
 
 sudo init_volumes
 
+mkdir -p /opt/solr/server/solr/configsets
+
+# Symlinks config sets to volume.
+for configset in $(ls -d /opt/docker-solr/configsets/*); do
+    if [[ ! -d "/opt/solr/server/solr/configsets/${configset##*/}" ]]; then
+        ln -s "${configset}" /opt/solr/server/solr/configsets/;
+    fi
+done
+
 if [[ ! -f /opt/solr/server/solr/solr.xml ]]; then
     ln -s /opt/docker-solr/solr.xml /opt/solr/server/solr/solr.xml
 fi
