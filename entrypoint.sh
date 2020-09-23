8098c4485ad98404234d30312bae384227b05a46
Brad Jones <brad.jones@fruition.net>
2020-09-23 10:59:13 -1000

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-init-var-solr
+# Don't recursively chown since it is very slow, and this is only necessary on
+# first start.
+chown solr:solr /var/solr
 
 gosu solr docker-entrypoint.sh "$@"
